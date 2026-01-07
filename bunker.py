f611bfe85c7f1659e6bef96274445afd021dab62
nixonwalcer-cpu <nixon.walcer@rbe.sk.ca>
2026-01-07 13:10:09 -0600

    Update bunker.py

---
+++ bunker.py
@@ -0,0 +1,28 @@
+#ONLY FOR NIXON
+
+map = [[bathroom, boiler],
+       [entrence, kitchen],
+       [hallway, bedroom]]
+ 
+
+
+
+
+boiler_room = room ("boiler room", "the boiler is broken and you need to," \
+                    +"fix it because its getting really cold........"),
+kitchen = room ("kitchen", "the kitchen has a hole in the ceiling ......."),
+bedroom = room ("bedroom", "the bedroom wall has a crack in  it.......")
+hallway = room ("hallway", "the hallway has a leaking pipe......" )
+bathroom = room ("bathroom", "the toilet is broken........")
+entrence = room ("entrence","the entrence is " )
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    #This code will not run during the game.
+    #This code is for just testing the bunker features.
+    print("World")
