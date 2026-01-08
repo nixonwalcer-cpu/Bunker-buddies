dc836ed9baa6d40d48ad29168046f04ba2a89627
nixonwalcer-cpu <nixon.walcer@rbe.sk.ca>
2026-01-08 13:15:38 -0600

    changes

--- bunker.py
+++ bunker.py
@@ -1,5 +1,15 @@
 #ONLY FOR NIXON
 
+class Room
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+    def __str__(self):
+        return self.name.title()
+
+    
+    
 map = [[bathroom, boiler],
        [entrence, kitchen],
        [hallway, bedroom]]
@@ -17,12 +27,13 @@ bathroom = room ("bathroom", "the toilet is broken........")
 entrence = room ("entrence","the entrence is " )
 
 
+print(map[0][1])
 
 
 
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #This code will not run during the game.
     #This code is for just testing the bunker features.
     print("World")
