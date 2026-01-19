fe9ff117014c92f68917024abe611bd14db709cc
brodynimmo-byte <brody.nimmo@rbe.sk.ca>
2026-01-19 13:32:30 -0600

    iqulegh;uchal

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import player as p
+#########################################################################################
+# Title:Bunker budies
+# Names(s): Brody, Nixon
+# Class: CS 30
+# Date:1/16/26
+# Version: I don't know we forgot to count
+#########################################################################################
+"""
+Our CS 30 fianl project, a game called bunker budies. This here just starts the game.
+"""
+#########################################################################################
+# Impoorts and global variables ---------------------------------------------------------
+import player as p 
+import bunker2 as b 
 
+# Functions -----------------------------------------------------------------------------
 def game():
-    p.playing()
+    p.playing()#playes house section
+    b.bunker_menu()#playes bunker section
+
 
 if __name__ == "__main__":
     game()
